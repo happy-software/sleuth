7ad8dc779d96a74f7776938cf6ff133e963b0c48
Hector Rios <that.hector@gmail.com>
2021-08-11 09:25:25 -0500

    Update migration to encrypt more attrs
    This commit changes the user schema to encrypt more attributes.
    
    We will be only using blind indexing for first name, last name and
    email.
    
    Email should use a blind index because it is the identifier that the
    user will use to log in. Without this, the login flow would be a pain.
    
    First and Last name both will leverage a blind index so that we can
    query data brokers for exact matches. If we needed something like
    figuring out the correct age + state, we can further drill down in
    memory.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  encrypts :first_name, :last_name, :email
+  encrypts :first_name, :last_name, :state, :email
+  encrypts :age, type: :integer
+
+  blind_index :first_name, slow: true
+  blind_index :last_name, slow: true
   blind_index :email, slow: true
 end
--- db/migrate/20210809023402_create_users.rb
+++ db/migrate/20210809023402_create_users.rb
@@ -1,15 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      # t.text :first_name
-      # t.text :last_name
-      # t.text :email
-      t.text :state
-
-      # We don't encrypt state because :shurg:
       t.text :first_name_ciphertext
       t.text :last_name_ciphertext
       t.text :email_ciphertext
+      t.text :state_ciphertext
+      t.text :age_ciphertext
+
+      t.text :first_name_bidx, index: true
+      t.text :last_name_bidx, index: true
       t.text :email_bidx, index: { unique: true }
 
       t.timestamps
