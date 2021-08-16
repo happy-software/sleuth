d2b095d7d01ab5881a1e3b74fb9efa17ebb82562
Hector Rios <that.hector@gmail.com>
2021-08-15 22:50:16 -0500

    Add UserDataBroker join model to tie User<>DataBroker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_164402) do
+ActiveRecord::Schema.define(version: 2021_08_16_032142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2021_08_13_164402) do
     t.text "notes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_data_brokers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "data_broker_id"
+    t.text "status"
+    t.datetime "opted_out_on"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["data_broker_id"], name: "index_user_data_brokers_on_data_broker_id"
+    t.index ["user_id"], name: "index_user_data_brokers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
